f34545199a52f6050978128e980d987e7147ba12
Vladislav <vladislav.shvorobuk@gmail.com>
2018-12-11 21:51:32 +0200

    try make draf&drop

--- dist/script.js
+++ dist/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         let body = document.body;
         let fragment = document.createDocumentFragment();
+        // let modal = document.createDocumentFragment("div");
+        let div = document.createElement("div");
 
         let search = document.createElement("button");
         let next = document.createElement("button");
@@ -20,21 +22,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
         searchPrevNodeEl();
         searchParentNodeEl();
         searchChildNodeEl();
+        // dragAndDrop();
+        
     }
 
     main();
 
     function renderBlock() {
       
-        let div = document.createElement("div");
-
         div.style.position = "fixed";
         div.style.padding = "20px";
         div.style.top = "20px";
         div.style.right = "1%";
         div.style.border = "5px solid grey";
         div.style.width ="270px";
-        div.style.background = "rgb(128,128,128, 0.5)";
+        div.style.background = "rgb(128,128,128, 0.9)";
+        div.style.zIndex = "20";
 
         let h3 = document.createElement("h3");
         h3.innerHTML = "Search node element";
@@ -50,7 +53,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         input.style.marginRight ="5px";
         input.style.marginBottom ="5px";
         input.style.border = "1px solid grey";
-        input.style.width ="100%";        
+        input.style.width ="100%";   
+        // input.setAttribute("onmousedown", "return false");        
 
         div.appendChild(input);
 
@@ -63,6 +67,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         search.style.display = "block";
         search.style.width ="100%"; 
         search.style.marginBottom ="5px";   
+        search.style.zIndex ="100";   
+        // search.removeEventListener("mousedown", pushMouseDown);   
 
         // let next = document.createElement("button");
         // next.setAttribute("class", "btn");
@@ -72,6 +78,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         next.style.marginBottom ="5px";
         next.style.width = "48.3%";
         next.disabled = true;
+
 
         // let previous = document.createElement("button");
         // previous.setAttribute("class", "btn");
@@ -100,16 +107,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
         // style.color = "red";
 
         div.appendChild(search);
-
         div.appendChild(buttons);
 
         buttons.appendChild(next);
         buttons.appendChild(previous);
         buttons.appendChild(parent);
         buttons.appendChild(child);
-
-
-        
+      
         fragment.appendChild(div);
         body.appendChild(fragment);
        
@@ -118,8 +122,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     function searchNodeEl () {
         
-        search.addEventListener("click", function () {
+        search.addEventListener("click", function (event) {
             event.preventDefault();
+            // alert("asdasdsd");
 
             let text = input.value;
             input.value = "";
@@ -208,9 +213,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 document.querySelector(".redBorder").style.border = "4px solid red";
                 }
             }
-            checkDisable(elem);
-            console.log(elem);
-           
+            checkDisable(elem);        
         });
         
     }
@@ -239,6 +242,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         });
         
     }
+
     function searchChildNodeEl () {
         
         child.addEventListener("click", function () {
@@ -266,13 +270,56 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 
- 
+function dragAndDrop () {
+
+    div.onmousedown = function(e) {
+    
+        let coords = getCoords(div);
+        let shiftX = e.pageX - coords.left;
+        let shiftY = e.pageY - coords.top;
+        
+        div.style.position = 'absolute';
+        moveAt(e);
+    
+        body.appendChild(div);
+    
+    
+        function moveAt(e) {
+            div.style.left = e.pageX - shiftX + 'px';
+            div.style.top = e.pageY - shiftY + 'px';
+          }
+    
+        document.onmousemove = function(e) {
+            moveAt(e);
+        }
+    
+          div.onmouseup = function() {
+            document.onmousemove = null;
+            div.onmouseup = null;
+          }
+    
+    
+      div.ondragstart = function() {
+        return false;
+      };
+    
+      function getCoords(elem) { 
+        let box = elem.getBoundingClientRect();
+        return {
+          top: box.top + pageYOffset,
+          left: box.left + pageXOffset
+        };
+      }
+    }
 
 
-
-
+}
 
  
 
    
+
+
+
+    
 });
